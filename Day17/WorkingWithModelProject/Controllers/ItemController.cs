4b274efaf1deafd2df23f80a577a6dbdda76aed4
Gayathri Mahadevan <gayat_19@yahoo.com>
2021-06-22 22:55:02 +0530

    Day17

---
+++ Day17/WorkingWithModelProject/Controllers/ItemController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorkingWithModelProject.Models;
+
+namespace WorkingWithModelProject.Controllers
+{
+    public class ItemController : Controller
+    {
+        static List<Item> items = new List<Item>();
+        public IActionResult Index()
+        {
+            return View(items);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Item item)
+        {
+            items.Add(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Details(int id)
+        {
+            Item item;
+            try
+            {
+                item = items.Single(itm => itm.Id == id);
+            }
+            catch(ArgumentNullException ane)
+            {
+                item = null;
+            }
+            catch(InvalidOperationException ioe)
+            {
+                item = null;
+            }
+            return View(item);
+        }
+        public IActionResult Delete(int id)
+        {
+            Item item;
+            try
+            {
+                item = items.Single(itm => itm.Id == id);
+            }
+            catch (ArgumentNullException ane)
+            {
+                item = null;
+            }
+            catch (InvalidOperationException ioe)
+            {
+                item = null;
+            }
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Delete(int id,Item item)
+        {
+            items.Remove(item);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Edit(int id)
+        {
+            Item item;
+            try
+            {
+                item = items.Single(itm => itm.Id == id);
+            }
+            catch (ArgumentNullException ane)
+            {
+                item = null;
+            }
+            catch (InvalidOperationException ioe)
+            {
+                item = null;
+            }
+            return View(item);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, Item item)
+        {
+            Item myItem = items.Single(itm => itm.Id == id);
+            myItem.Name = item.Name;
+            myItem.Price = item.Price;
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Day17/WorkingWithModelProject/Models/Item.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WorkingWithModelProject.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public override bool Equals(object obj)
+        {
+            return this.Id.Equals(((Item)obj).Id);
+        }
+    }
+}
